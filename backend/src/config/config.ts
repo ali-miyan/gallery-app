c28312e7b7c40835292c42ae7472302597f10b95
ali-miyan <alimiyan1112@gmail.com>
2024-09-16 17:07:53 +0530

    updated mongourl

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/galleryApp')
+    await mongoose.connect('mongodb+srv://alimiyan1112:Aylanesa7@cluster0.yu4ra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,29 @@
-import express, { Express } from 'express';
-import userRoute from './routes/userRoute';
-import { pageNotFound, errorHandler } from './middleware/errorMiddleware';
-import connectDB from './config/config';
-const app: Express = express();
-import cors from 'cors';
-import cookie from 'cookie-parser';
-connectDB()
+    import express, { Express } from 'express';
+    import userRoute from './routes/userRoute';
+    import { pageNotFound, errorHandler } from './middleware/errorMiddleware';
+    import connectDB from './config/config';
+    const app: Express = express();
+    import cors from 'cors';
+    import cookie from 'cookie-parser';
+    connectDB()
 
-const port = process.env.PORT || 5000;
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true
-}));
-app.use(cookie())
-
-app.use('/api/user', userRoute)
-
-app.use(pageNotFound)
-app.use(errorHandler)
+    const port = process.env.PORT || 5000;
 
 
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
-});
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }))
+    app.use(cors({
+        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        credentials: true
+    }));
+    app.use(cookie())
+    
+    app.use('/api/user', userRoute)
+
+    app.use(pageNotFound)
+    app.use(errorHandler)
+
+
+    app.listen(port, () => {
+        return console.log(`Express is listening at http://localhost:${port}`);
+    });
