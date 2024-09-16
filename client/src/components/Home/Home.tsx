290af97181fc89fe2153792393edf2398e25c17e
ali-miyan <alimiyan1112@gmail.com>
2024-09-16 22:32:37 +0530

    path changed in frontend

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -237,7 +237,7 @@ const Home = () => {
         <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
           <div className="relative max-w-4xl max-h-full">
             <img
-              src={`../../../public/userImages/${imageToView}`}
+              src={`/userImages/${imageToView}`}
               alt="Full screen"
               className="max-w-full max-h-[90vh] object-contain"
             />
