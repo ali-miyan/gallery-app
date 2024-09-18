13b865b1ba17a17fdf80aa5ab14e54eca144c779
ali-miyan <alimiyan1112@gmail.com>
2024-09-18 10:10:43 +0530

    added path

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,1 +1,1 @@
-export const baseUserUrl = import.meta.env.VITE_URL || "http://localhost:5000/api/user";
+export const baseUserUrl = import.meta.env.VITE_URL ? `${import.meta.env.VITE_URL}/api/user` : "http://localhost:5000/api/user";
